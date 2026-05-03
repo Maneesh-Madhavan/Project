fc152dd2d901867cc89d8505aab48e3088c416c3
maneesh <sonofms2002@gmail.com>
2026-05-03 10:45:35 +0530

    render issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Forms from "./components/Forms";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import RoomPage from "./pages/RoomPage";
 import io from "socket.io-client";
